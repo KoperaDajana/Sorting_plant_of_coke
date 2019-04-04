54223c0fa717c31547cde3ea138dab69b9ab2bef
Dajana <32611274+KoperaDajana@users.noreply.github.com>
2019-04-04 16:24:31 +0200

    Delete wagon.js

--- Kody aplikacji/api/models/wagon.js
+++
@@ -1,9 +0,0 @@
-// MODEL Wagonu, do niego zostają usypywane kopy koksu, posiada dwa atrybuty - id i etykietę,
-// etykieta posiada informacje o wagonie
-const mongoose = require('mongoose');
-
-const wagonSchema = mongoose.Schema({
-    label: {type: mongoose.Schema.Types.ObjectId, ref: 'Label', required: true}
-});
-
-module.exports = mongoose.model('Wagon', wagonSchema);
