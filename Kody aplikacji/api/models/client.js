3557d8e38c038050894e523a5e4be8f9d1994ed6
Dajana <32611274+KoperaDajana@users.noreply.github.com>
2019-04-04 16:23:44 +0200

    Delete client.js

--- Kody aplikacji/api/models/client.js
+++
@@ -1,9 +0,0 @@
-// MODEL Klient, informacje o kliencie, który złożył zamówienie (id oraz nazwa klienta)
-const mongoose = require('mongoose');
-
-// powie jak powinien wyglądać klient
-const clientSchema = mongoose.Schema({
-    client_name: {type: String, required: true}
-});
-
-module.exports = mongoose.model('Client', clientSchema);
